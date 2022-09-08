2918e41c3fb2285fabfe64b8dc247599ffb3df14
TjulienP <107543248+TjulienP@users.noreply.github.com>
2022-09-08 16:55:44 +0200

    fix:fin back patient
    fix; Cross origin dnas le controller

--- src/main/java/com/example/cabinetmedicalback/controller/PatientController.java
+++ src/main/java/com/example/cabinetmedicalback/controller/PatientController.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/Patient")
+@CrossOrigin
 public class PatientController {
 
     private final PatientService service;
@@ -57,5 +58,5 @@ public class PatientController {
         return this.service.deletePatient(id);
     }
 
-    
+
 }
